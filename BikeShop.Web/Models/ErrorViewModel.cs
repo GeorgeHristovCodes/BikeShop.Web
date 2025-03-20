2ae97a58a4fb5b0ddca1ef8ab27cc22485fb36ce
GeorgeHristovCodes <georgihristov045@yahoo.com>
2025-03-20 08:51:42 +0200

    Initial commit

---
+++ BikeShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BikeShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
